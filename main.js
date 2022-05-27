9e9bbf5c93fa0a81d612c8354f208a9809c21f4a
codeytimey <87842664+codeytimey@users.noreply.github.com>
2022-05-27 12:29:39 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas=createCanvas(550, 500);
+    canvas.position(560, 90);
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+    }
+}
+
+function draw() {
+    background("grey");
+}
